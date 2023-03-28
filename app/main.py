6cea8451fd688e7a7f1ead4a40d0edfd19279779
italoooliveira <italo.oli12@gmail.com>
2023-03-28 16:10:50 -0300

    Init

---
+++ app/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+import mysql.connector
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/employees")
+async def get_employees():
+    connection = mysql.connector.connect(
+        user='root',
+        password='RootPassword',
+        host='mysql',
+        port='3306',
+        database='Company'
+    )
+    print('DB connected')
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute('Select * FROM employees')
+    employees = cursor.fetchall()
+    datas = []
+    for employee in employees:
+        print(employee)
+        data = {
+            "first_name": employee["first_name"],
+            "last_name": employee["last_name"],
+            "email": employee["email"],
+            "department": employee["department"]
+        }
+        datas.append(data)
+    connection.close()
+    print('DB closed')
+    return { 'employees' : datas }
